189238300122b39ac8cf3f9fc09b3908c621a5eb
saelozahra <saelozahra14@gmail.com>
2023-04-10 17:39:33 +0430

    back to sqlite db

--- movasat/settings.py
+++ movasat/settings.py
@@ -82,6 +82,7 @@ WSGI_APPLICATION = 'movasat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,29 +90,36 @@ DATABASES = {
     }
 }
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'PGDB',
-#             'USER': 'POSTGRESUsr',
-#             'PASSWORD': 'passwordRAZ5224',
-#             'HOST': 'localhost',
-#             'PORT': '5434',
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'PGDB',
-#             'USER': 'POSTGRESUsr',
-#             'PASSWORD': 'passwordRAZ5224',
-#             'HOST': 'movasat_postgresql',
-#             'PORT': '5432',
-#         }
-#     }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'mongo',
+    }
+}
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'PGDB',
+            'USER': 'POSTGRESUsr',
+            'PASSWORD': 'passwordRAZ5224',
+            'HOST': 'localhost',
+            'PORT': '5434',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'PGDB',
+            'USER': 'POSTGRESUsr',
+            'PASSWORD': 'passwordRAZ5224',
+            'HOST': 'movasat_postgresql',
+            'PORT': '5432',
+        }
+    }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
