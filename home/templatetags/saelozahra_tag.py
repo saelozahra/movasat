a63fc475c5e71e6b650b901c5072c7bb18bdff48
saelozahra <saelozahra14@gmail.com>
2023-06-02 13:34:33 +0330

    add thusent tag

--- home/templatetags/saelozahra_tag.py
+++ home/templatetags/saelozahra_tag.py
@@ -1,10 +1,13 @@
 from django import template
+from django.utils.safestring import mark_safe
+
 register = template.Library()
 
 
+@mark_safe
 def currency(dollars):
     # dollars = round(float(dollars), 2)
-    return f"{dollars:,} تومان"
+    return f"<b>{dollars:,}</b> تومان"
 
 
 register.filter('currency', currency)
