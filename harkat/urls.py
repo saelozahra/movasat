808e8816471e145f9f92d74131083ed8904543d2
saelozahra <saelozahra14@gmail.com>
2023-06-02 15:17:20 +0330

    افزودن صفحه داخلی حرکت ها

--- harkat/urls.py
+++ harkat/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from harkat import views
 urlpatterns = [
+    path('harkat/', views.harkat_page, name='harkat_page'),
     path('harkat/<jahadi>/<slug>/', views.harkat_single, name='harkat_single'),
 ]
--- harkat/views.py
+++ harkat/views.py
@@ -3,10 +3,13 @@ from harkat.models import Harkat
 
 # Create your views here.
 
+
+def harkat_page(request):
+
+    data = Harkat.objects.filter().all()
+    return render(request, "harkat_page.html", context={"harkat": data})
+
+
 def harkat_single(request, jahadi, slug):
-    harkatdata = Harkat.objects.filter(slug=slug).get()
-    data = {
-        "data": harkatdata,
-        "percent_amount": int(percentage(harkatdata.Amount, harkatdata.TotalAmount))
-    }
-    return render(request, "harkat_page.html", context={"data": data})
+    data = Harkat.objects.filter(slug=slug).get()
+    return render(request, "harkat_item.html", context={"harkat": data})
