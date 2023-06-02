f04ece62d424bbf850d64cc44c7769ae34940554
saelozahra <saelozahra14@gmail.com>
2023-06-03 00:43:22 +0330

    افزودن دسته بندی کرادفاندینگ ها

--- harkat/urls.py
+++ harkat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from harkat import views
 urlpatterns = [
     path('harkat/', views.harkat_page, name='harkat_page'),
+    path('harkat/cat/<cid>', views.harkat_page, name='harkat_cat'),
     path('harkat/<jahadi>/<slug>/', views.harkat_single, name='harkat_single'),
 ]
--- harkat/views.py
+++ harkat/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from harkat.models import Harkat
+from harkat.models import CrowdFunding
 
 # Create your views here.
 
 
 def harkat_page(request):
 
-    data = Harkat.objects.filter().all()
+    data = CrowdFunding.objects.filter().all()
     return render(request, "harkat_page.html", context={"harkat": data})
 
 
 def harkat_single(request, jahadi, slug):
-    data = Harkat.objects.filter(slug=slug).get()
+    data = CrowdFunding.objects.filter(slug=slug).get()
     return render(request, "harkat_item.html", context={"harkat": data})
