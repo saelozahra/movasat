4fe09be43d39a623fff45ed8bc57c907a047c5a5
saelozahra <saelozahra14@gmail.com>
2023-06-12 21:52:07 +0330

    add project to forum.html

--- challenge/urls.py
+++ challenge/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('challenge/<fid>', views.show_forum, name='show_forum'),
+    # path('challenge/tag/<c_cat_slug>', views.show_challenge_cat, name='show_challenge_cat'),
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -1,3 +1,4 @@
+from harkat.models import Project
 from django.shortcuts import render
 from django.http import Http404
 from .models import Forum, Response
@@ -7,6 +8,7 @@ from .models import Forum, Response
 def show_forum(request, fid):
     try:
         context = {
+            "project": Project.objects.filter(id=fid).get(),
             "forum": Forum.objects.filter(id=fid).get(),
             "response": Response.objects.filter(RelatedForum_id=fid).all(),
         }
