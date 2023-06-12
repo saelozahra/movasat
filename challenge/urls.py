7f8d54ff01b9d689d4b8052d8edd88b8cc6754fb
saelozahra <saelozahra14@gmail.com>
2023-06-13 01:48:37 +0330

    add save response

--- challenge/urls.py
+++ challenge/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('challenge/<fid>', views.show_forum, name='show_forum'),
+    path('challenge/<fid>', views.ForumView.as_view(), name='show_forum'),
     # path('challenge/tag/<c_cat_slug>', views.show_challenge_cat, name='show_challenge_cat'),
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -1,17 +1,34 @@
+from django.urls import reverse
+from django.views.generic import TemplateView
 from harkat.models import Project
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from .models import Forum, Response
 # Create your views here.
 
 
-def show_forum(request, fid):
-    try:
-        context = {
-            "project": Project.objects.filter(id=fid).get(),
-            "forum": Forum.objects.filter(id=fid).get(),
-            "response": Response.objects.filter(RelatedForum_id=fid).all(),
-        }
-    except Forum.DoesNotExist:
-        raise Http404
-    return render(request, "forum.html", context=context)
+class ForumView(TemplateView):
+    def post(self, request, **kwargs):
+        uid = request.POST["uid"]
+        fid = request.POST["fid"]
+        description = request.POST["description"]
+        file = request.POST["file"]
+        Response.objects.create(
+            RelatedForum_id=fid,
+            description=description,
+            file=file,
+            writer_id=uid,
+        )
+        return HttpResponseRedirect(reverse("show_forum", kwargs={"fid": fid, }))
+
+    def get(self, request, **kwargs):
+        fid = kwargs.get("fid")
+        try:
+            context = {
+                "project": Project.objects.filter(id=fid).get(),
+                "forum": Forum.objects.filter(id=fid).get(),
+                "response": Response.objects.filter(RelatedForum_id=fid).all(),
+            }
+        except Forum.DoesNotExist:
+            raise Http404
+        return render(request, "forum.html", context=context)
