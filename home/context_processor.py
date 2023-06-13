c07e0b503abcb6c70bda1d1fd341b158f91920b0
saelozahra <saelozahra14@gmail.com>
2023-06-13 12:23:41 +0330

    add challenge to home

--- home/context_processor.py
+++ home/context_processor.py
@@ -2,6 +2,7 @@
 def site(request):
     context = {
         "site_name": "قرارگاه جهادی شهید حاج قاسم سلیمانی",
-        "home_url": "http://jahadi.hajghasem.ir",
+        # "home_url": "http://jahadi.hajghasem.ir",
+        "home_url": "http://127.0.0.1:8004",
     }
     return context
