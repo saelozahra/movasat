da3bc0ff9aba7d622bf445af0314837c6a0b8f83
saelozahra <saelozahra14@gmail.com>
2023-06-13 14:28:24 +0330

    add forum to home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from challenge.models import Forum
 from harkat.models import CrowdFunding
 from lms.models import Course
 from madadyar.models import JahadActivity
@@ -11,10 +12,12 @@ def home_view(request):
     context = {
         'harkat': CrowdFunding.objects.all(),
         'course': Course.objects.all(),
+        'forum': Forum.objects.all(),
         'statistics': {
             'lms': f"{Course.objects.all().count():,}",
             'harkat': f"{CrowdFunding.objects.all().count():,}",
             'madadyar': f"{JahadActivity.objects.all().count():,}",
+            'forum': f"{Forum.objects.all().count():,}",
         },
     }
     return render(request, 'home.html', context)
