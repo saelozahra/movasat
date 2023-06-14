335e0c2574db9ad16393adabef9b447e258b365e
saelozahra <saelozahra14@gmail.com>
2023-06-15 01:03:07 +0330

    Divar models.py

--- challenge/models.py
+++ challenge/models.py
@@ -1,6 +1,7 @@
-from urllib import request
-
-import requests
+import sys
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
@@ -16,6 +17,7 @@ class Forum(models.Model):
     title = models.CharField(max_length=202, null=False, verbose_name="عنوان چالش")
     description = RichTextField(blank=False, null=False, verbose_name="توضیحات چالش")
     cover = models.ImageField(blank=True, upload_to="files/challenge/%Y/%m/", verbose_name="تصویر")
+    thumbnail = models.ImageField(upload_to='files/challenge/%Y/%m/', editable=False, blank=True, verbose_name='تصویرک')
     file = models.FileField(blank=True, upload_to="files/challenge/%Y/%m/", verbose_name="فایل")
     RelatedProject = models.ForeignKey(Project, blank=True, on_delete=models.DO_NOTHING, verbose_name="پروژه مربوطه")
     owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="مطرح کننده")
@@ -30,6 +32,21 @@ class Forum(models.Model):
 
     def get_absolute_url(self):
         return reverse("show_forum", kwargs={"fid": self.id, })
+
+    def save(self, *args, **kwargs):
+        output_size = (313, 313)
+        output_thumb = BytesIO()
+
+        img = Image.open(self.cover)
+        img_name = self.cover.name.split('.')[0]
+
+        if img.height > 313 or img.width > 313:
+            img.thumbnail(output_size)
+            img.save(output_thumb, format='JPEG', quality=90)
+
+        self.thumbnail = InMemoryUploadedFile(output_thumb, 'ImageField', f"{img_name}_thumb.jpg", 'image/jpeg',
+                                              sys.getsizeof(output_thumb), None)
+        return super().save(*args, **kwargs)
 
 
 class Response(models.Model):
@@ -46,6 +63,48 @@ class Response(models.Model):
     def __str__(self):
         return f"پاسخ {self.id}، {self.writer.get_full_name()} به {self.RelatedForum}"
 
-    # def save(self, *args, **kwargs):
-    #     if self.writer is None:
-    #     return super().save(*args, **kwargs)
+
+class Divar(models.Model):
+    StatusChoices = (
+        (1, 'آزاد ✅'),
+        (2, 'در حال استفاده 🔄'),
+        (3, 'امانت داده شده 💠'),
+    )
+
+    TypeChoices = (
+        (1, 'امانت دادن'),
+        (0, 'نیازمندی'),
+    )
+
+    Type = models.BooleanField(choices=TypeChoices, verbose_name="نوع آگهی", default=1,
+                               help_text="به این کالا برا کار خیرتون نیاز دارید؟ یا میخواید به یکی دیگه امانتش بدید؟")
+    Name = models.CharField(max_length=202, null=False, verbose_name="نام")
+    Description = models.TextField(null=False, verbose_name="متن")
+    Picture = models.ImageField(blank=True, upload_to="files/divar/%Y/%m/", verbose_name="تصویر")
+    Thumbnail = models.ImageField(upload_to='files/divar/%Y/%m/', editable=False, blank=True, verbose_name='تصویرک')
+    SubmitDate = jmodels.jDateTimeField(auto_now=True, verbose_name="زمان ثبت")
+    Status = models.PositiveSmallIntegerField(choices=StatusChoices, verbose_name="وضعیت")
+    Warranty = models.CharField(max_length=313, blank=True, help_text="برای ضمانت چی میخواین ؟", verbose_name="ضمانت")
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="مالک")
+
+    def __str__(self):
+        return self.Name
+
+    class Meta:
+        verbose_name = "دیوار مهربانی"
+        verbose_name_plural = "دیوار مهربانی"
+
+    def save(self, *args, **kwargs):
+        output_size = (313, 313)
+        output_thumb = BytesIO()
+
+        img = Image.open(self.Picture)
+        img_name = self.Picture.name.split('.')[0]
+
+        if img.height > 313 or img.width > 313:
+            img.thumbnail(output_size)
+            img.save(output_thumb, format='JPEG', quality=90)
+
+        self.Thumbnail = InMemoryUploadedFile(output_thumb, 'ImageField', f"{img_name}_thumb.jpg", 'image/jpeg',
+                                              sys.getsizeof(output_thumb), None)
+        return super().save(*args, **kwargs)
