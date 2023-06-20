36d0178a151931c2ffee08d50048f63d64b99707
saelozahra <saelozahra14@gmail.com>
2023-06-20 18:26:34 +0330

    add register link

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('auth/otp/validate', OTPValidate.as_view(), name='OTPValidate'),
     # path(r'', views.MainPage.as_view(), name="index"),
     # path('page/<slug>', views.page_view, name='page_view'),
-    path('madadkar/<user>', get_profile, name='get_profile')
+    path('madadkar/<user>', get_profile, name='get_profile'),
+    path('register', UserCreate.as_view(), name='UserCreate')
 ]
