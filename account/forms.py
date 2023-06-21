a2792610745b966df2376857f024374388d4bd58
saelozahra <saelozahra14@gmail.com>
2023-06-22 00:44:33 +0330

    فرم ثبت نام

--- account/forms.py
+++ account/forms.py
@@ -1,21 +1,17 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import UserDetail
 
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-
 class RegisterUser(forms.ModelForm):
-    def __int__(self, *args, **kwargs):
-        super(RegisterUser, self).__init__(*args, **kwargs)
 
-        self.fields["tel"].disabled = True
-        self.fields["melli"].disabled = True
+    class Meta:
+        model = UserDetail
+        # fields = '__all__'
+        fields = ['first_name', 'last_name', 'username', 'tel', 'email', 'melli', 'password', 'City', 'birth', 'skills', ]
 
-        class Meta:
-            model = UserDetail
-            fields = ['tel', 'City', 'birth', 'skills', 'melli']
+
+class ActiveUser(forms.ModelForm):
+    otp = forms.IntegerField(label="کد ورود")
+
