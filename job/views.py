3c2af7c9942b520fcb2d9c161c59adb890c925ee
saelozahra <saelozahra14@gmail.com>
2023-06-24 02:31:54 +0330

    single job view

--- job/views.py
+++ job/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 
-def job(request):
+def all_job(request):
     get_jobs = Job.objects.all()
     return render(request, "", context={'data': get_jobs})
+
+
+def job(request, jowner, jid):
+    job = Job.objects.filter(id=jid).get()
+    return render(request, "JobSingle.html", context={'job': job})
