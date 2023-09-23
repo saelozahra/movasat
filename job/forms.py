36a8a2b59ea53c7e92d9be5f865a365d5347e4cb
saelozahra <saelozahra14@gmail.com>
2023-09-23 13:19:06 +0330

    reg job box

--- job/forms.py
+++ job/forms.py
@@ -1,3 +1,4 @@
+import home.models
 from django import forms
 from location_field.forms.plain import PlainLocationField
 
@@ -5,6 +6,14 @@ from .models import Job
 
 
 class RegisterJob(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+
+        try:
+            self.register_job = home.models.Box.objects.filter(location="register_job").get()
+        except home.models.Box.DoesNotExist:
+            self.register_job = ""
+
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Job
